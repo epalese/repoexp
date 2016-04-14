fb1883389400049578faf59ddf41bcb317cb5f9e
Emanuele Palese <epalese@TAG-123.local>
2016-04-14 14:29:39 +0100

    bug fixing

--- webapp/js/views/paragraphs/edit.jsx
+++ webapp/js/views/paragraphs/edit.jsx
@@ -115,7 +115,7 @@ export default React.createClass({
                     //     //     label: 'somethingA',
                     //     //     values: [{x: 'SomethingA', y: 10}, {x: 'SomethingB', y: 4}, {x: 'SomethingC', y: 3}]
                     //     // }
-                    ];
+                    // ];
 
                     var dataSet = window[variable];
                     var values = dataSet.map(function(e) {
